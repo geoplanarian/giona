85286b440b7fe34923ac2e95e4e18ca55da8909d
Andrew Mallamo <andrewmallamo@gmail.com>
2018-12-07 19:49:41 -0500

    2nd

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-<v-navigation-drawer
-      fixed
+  <v-app id="giona">
+    <v-navigation-drawer
       v-model="drawer"
+      fixed
       app
     >
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile >
           <v-list-tile-action>
             <v-icon>home</v-icon>
           </v-list-tile-action>
@@ -13,7 +14,7 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="">
+        <v-list-tile >
           <v-list-tile-action>
             <v-icon>contact_mail</v-icon>
           </v-list-tile-action>
@@ -30,77 +31,30 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout
-          justify-center
-          align-center
+
         >
-          <v-flex text-xs-center>
-          <section class="container">
-            <h2>giona</h2>
-            <ul>
-              <li v-for="post in posts" :key="post.date">
-                <nuxt-link :to="post._path"> {{ post.title }} </nuxt-link>
-              </li>
-            </ul>
-          </section>
+          <v-flex>
           </v-flex>
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer color="indigo" app inset>
+    <v-footer color="indigo" app>
       <span class="white--text">&copy; 2017</span>
     </v-footer>
-
+  </v-app>
 </template>
 
 <script>
-import AppLogo from "~/components/AppLogo.vue";
 
-export default {
-  components: {
-    AppLogo
-  },
-  data() {
-    // Using webpacks context to gather all files from a folder
-    const context = require.context("~/content/blog/posts/", false, /\.json$/);
 
-    const posts = context.keys().map(key => ({
-      ...context(key),
-      _path: `/blog/${key.replace(".json", "").replace("./", "")}`
-    }));
-
-    return { posts };
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    components: {
+    },
+    props: {
+      source: String
+    }
   }
-};
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
